25f24eed0861fb21de12ddddb60bcc4d9e73d389
Joao M <jgo.martins@campus.fct.unl.pt>
2021-06-03 22:36:40 +0100

    Organized the projects directories and put things in the right place.
    Update to the processResults.py script with and argument option to remove outliers from the data before calculating an average value for the times.

--- Scripts/processResults.py
+++ Scripts/processResults.py
@@ -13,10 +13,10 @@ test_num = 9  # const
 workers = [1, 2, 4, 8, 16]
 runs = 5
 
-if len(sys.argv) != 2:
-    print("Usage: python3 processResults.py <file_name>")
+if len(sys.argv) != 3:
+    print("Usage: python3 processResults.py <remove_outliers> <file_name>")
 
-input_file = open(sys.argv[1], "r")
+input_file = open(sys.argv[2], "r")
 
 results = np.zeros((test_num, len(workers)))
 
@@ -27,7 +27,8 @@ for t in range(test_num):
             input_file.readline()
             times.append(float((input_file.readline().split(" ")[1]).split("\n")[0]))
             input_file.readline()
-        times = remove_outliers(times)
+        if sys.argv[1] == 1:
+            times = remove_outliers(times)
         avg = mean(times)
         results[t, w] = avg
 
