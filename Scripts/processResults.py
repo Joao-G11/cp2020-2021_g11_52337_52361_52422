25b220d77e28a0440eb2623820f21e030fc00e06
Bruno <bd.brito@campus.fct.unl.pt>
2021-06-05 03:49:29 +0100

    Details in Description:
    -Cluster Tests Output file added
    -Minor Change inpython script to process the cluster output data.
    -Excel with processed cluster outputs and Graphs added

--- Scripts/processResults.py
+++ Scripts/processResults.py
@@ -9,8 +9,8 @@ def remove_outliers(original):
     return original[1:-1]
 
 
-test_num = 9  # const
-workers = [1, 2, 4, 8, 16]
+test_num = 4  # const
+workers = [1, 2, 4, 8, 16, 32]
 runs = 5
 
 if len(sys.argv) != 2:
@@ -24,13 +24,13 @@ for t in range(test_num):
     for w in range(len(workers)):
         times = []
         for r in range(5):
-            input_file.readline()
             times.append(float((input_file.readline().split(" ")[1]).split("\n")[0]))
+            input_file.readline()
             input_file.readline()
         times = remove_outliers(times)
         avg = mean(times)
         results[t, w] = avg
 
-np.savetxt("local_results.csv", results, delimiter=",")
+np.savetxt("cluster_results.csv", results, delimiter=",")
 
 input_file.close()
